935111d2c1368b7bbdc649352e8a769e841d1217
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-19 22:19:49 +0200

    [ del ] card

---
+++ src/components/partials/TestimonialCard.vue
@@ -0,0 +1,48 @@
+<script>
+import { store } from '../../data/store';
+ export default {
+  name:'TestimonialCard',
+  data(){
+    return{
+      store
+    }
+  }
+}
+</script>
+
+<template>
+      <!-- slider -->
+        <div v-for="(card,index) in store.testimonials" 
+        :key="card.name + index" 
+        class="col-4 h-100">
+          <div class="card h-100">
+            <h5>{{ card.title }}</h5>
+            <p>{{ card.description }}</p>
+            <div class="profile d-flex">
+              <div class="avatar">
+                <img :src="`/public/images/${card.imgProfile}`" alt="">
+              </div>
+              <div class="profile-data">
+                <h6>{{ card.name }}</h6>
+                <span>{{ card.role }}</span>
+              </div>
+            </div>
+          </div>
+        </div>
+</template>
+
+
+<style lang="scss" scoped>
+  #testimonials {
+    background-color: rgb(250, 248, 246);
+      .card {
+        padding: 30px;
+        .profile {
+          img {
+            width: 50px;
+            border-radius: 50%;
+          }
+        }
+      }
+    }
+</style>
