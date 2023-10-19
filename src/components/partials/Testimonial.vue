a84d05020d1568b1386d16ea136b619e25ca34e9
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-19 22:19:36 +0200

    [ add ] component card testimonials

--- src/components/partials/Testimonial.vue
+++ src/components/partials/Testimonial.vue
@@ -1,61 +1,23 @@
 <script>
+import TestimonialCard from './TestimonialCard.vue';
  export default {
-  name:'Testimonial'
+  name:'Testimonial',
+  components:{
+    TestimonialCard
+  }
 }
 </script>
 
 <template>
       <!-- slider da fare componente -->
       <section id="testimonials">
-      <i>Testimonials</i>
-      <h4>Why do people love me?</h4>
+        <div class="title text-center ">
+          <i>Testimonials</i>
+          <h4>Why do people love me?</h4>
+        </div>
       <!-- slider -->
-      <div class="row w-100 justify-content-center ">
-        <div class="col-4">
-          <div class="card">
-            <h5>High level of eficiency and scientific teaching methods</h5>
-            <p>I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.</p>
-            <div class="profile d-flex">
-              <div class="avatar">
-                <img src="/public/images/artist-testimonial-avatar-04.jpg" alt="">
-              </div>
-              <div class="profile-data">
-                <h6>Name</h6>
-                <span>Role</span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-4">
-          <div class="card">
-            <h5>High level of eficiency and scientific teaching methods</h5>
-            <p>I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.</p>
-            <div class="profile d-flex">
-              <div class="avatar">
-                <img src="/public/images/artist-testimonial-avatar-04.jpg" alt="">
-              </div>
-              <div class="profile-data">
-                <h6>Name</h6>
-                <span>Role</span>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div class="col-4">
-          <div class="card">
-            <h5>High level of eficiency and scientific teaching methods</h5>
-            <p>I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.</p>
-            <div class="profile d-flex">
-              <div class="avatar">
-                <img src="/public/images/artist-testimonial-avatar-04.jpg" alt="">
-              </div>
-              <div class="profile-data">
-                <h6>Name</h6>
-                <span>Role</span>
-              </div>
-            </div>
-          </div>
-        </div>
+      <div class="row w-100 h-100 justify-content-center ">
+        <TestimonialCard/>
       </div>
     </section>
 </template>
@@ -64,14 +26,6 @@
 <style lang="scss" scoped>
   #testimonials {
     background-color: rgb(250, 248, 246);
-      .card {
-        padding: 30px;
-        .profile {
-          img {
-            width: 50px;
-            border-radius: 50%;
-          }
-        }
-      }
+      
     }
 </style>
