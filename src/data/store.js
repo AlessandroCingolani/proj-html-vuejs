03ed5530ec6d7667d32119ad84f6906ea878f48b
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-20 16:19:17 +0200

    [ add ] id

--- src/data/store.js
+++ src/data/store.js
@@ -130,6 +130,7 @@ export const store = reactive({
   ],
   testimonials: [
     {
+      id:0,
       name:'Mina Hollace',
       role:'/Freelancer',
       imgProfile:'artist-testimonial-avatar-04.jpg',
@@ -137,6 +138,7 @@ export const store = reactive({
       description:'I am free to learn at my own pace, follow my own schedule and choose the subject I want to learn from the syllabus. Great study portal for people like me.',
     },
     {
+      id:1,
       name:'Madley Pondor',
       role:'/IT Specialist',
       imgProfile:'artist-testimonial-avatar-01.jpg',
@@ -144,6 +146,7 @@ export const store = reactive({
       description:'I need to get a certification for English proficiency and MaxCoach is my best choice. Their tutors are smart and professional when dealing with students.',
     },
     {
+      id:2,
       name:'Florence Themes',
       role:'/Multimedia Admin',
       imgProfile:'artist-testimonial-avatar-02.jpg',
@@ -151,6 +154,7 @@ export const store = reactive({
       description:'l m a very strict person so I require everything to be organized and neat. Then, l Il be able to make things right and shina MaxCoach guys just got me.',
     },
     {
+      id:3,
       name:'Pinco Jenner',
       role:'/Ipsum Admin',
       imgProfile:'artist-testimonial-avatar-03.jpg',
