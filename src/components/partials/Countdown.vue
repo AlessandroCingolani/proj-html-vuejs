0493eb04df54602336d1e8130606235d658df1a8
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-21 10:52:27 +0200

    [ add ] component countdown

---
+++ src/components/partials/Countdown.vue
@@ -0,0 +1,32 @@
+<script>
+ export default {
+  name:'Countdown',
+  data() {
+    return {
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    }
+  },
+
+        // const remainingTime = endDate - currentDate;
+        // const seconds = Math.floor(remainingTime % 60);
+        // const minutes = Math.floor(remainingTime / 60);
+        // const hours = Math.floor(remainingTime / 60 / 60);
+        // const days = Math.floor(remainingTime / 60 / 60 / 24);
+}
+   
+</script>
+
+
+<template>
+  <h1>data</h1>
+</template>
+ 
+<style lang="scss" scoped>
+ @use '../../../scss/partials/general' as *;
+ @use '../../../scss/partials/vars' as *;
+
+ 
+</style>
