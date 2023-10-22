98bbb5bbcddc172e612cd428b25379e343021f53
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-22 22:54:24 +0200

    [ change ] position and padding

--- src/components/partials/ArtistCoaching.vue
+++ src/components/partials/ArtistCoaching.vue
@@ -8,31 +8,31 @@
   <!-- sezione 1 -->
   <section id="artist-coaching">
       <div class="container-fluid">
-        <div class="ac_sm-container text-center ">
-          <div class="title-section">
+        <div class="ac_sm-container">
+          <div class="title-section text-center">
             <em>Artist coaching</em>
             <h4>I understand what it takes to create.I can help you with</h4>
           </div>
           <div class="row">
             <div class="col">
               <img src="/public/images/artist-box-image-01.png" alt="">
-              <h6>Online Coures</h6>
-              <p class="">Online art coaching now offers you a very powerful way to redesign your artist mind.</p>
+              <h6>Online Courses</h6>
+              <p>Online art coaching now offers you a very powerful way to redesign your artist mind.</p>
             </div>
             <div class="col">
               <img src="/public/images/artist-box-image-02.png" alt="">
-              <h6>One to One</h6>
-              <p class="">Getting the necessary clarity about the current state to help you improve your ability.</p>
+              <h6 class="pt-3">One to One</h6>
+              <p>Getting the necessary clarity about the current state to help you improve your ability.</p>
             </div>
             <div class="col">
               <img src="/public/images/artist-box-image-03.png" alt="">
               <h6>Anywhere</h6>
-              <p class="">Access to valuable and portable program which allow you to setup and live anywhere you want.</p>
+              <p>Access to valuable and portable program which allow you to setup and live anywhere you want.</p>
             </div>
             <div class="col">
               <img src="/public/images/artist-box-image-04.png" alt="">
               <h6>On Time</h6>
-              <p class="">Punctuality is our top priority because it's an essential criteria to assess a program quality.</p>
+              <p>Punctuality is our top priority because it's an essential criteria to assess a program quality.</p>
             </div> 
           </div>
         </div>
@@ -50,5 +50,14 @@
     background-image: url('/public/images/maxcoach-shape-14-1536x343.png');
     background-position: center;
     background-size: cover;
+    h6 {
+      font-size: 1.1rem;
+      font-weight: 600;
+      padding-top: 10px;
+    }
+    p {
+      font-size: .9rem;
+      color: gray;
+    }
   }
 </style>
