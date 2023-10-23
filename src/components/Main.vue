9780e1ce51288f7ae5fe297afdfb9b69ab8eeb6c
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-23 12:45:57 +0200

    [ add ] components news and work together

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -6,6 +6,8 @@ import Jumbotron from './partials/Jumbotron.vue';
 import Testimonial from './partials/Testimonial.vue';
 import LatesCourses from './partials/LatesCourses.vue';
 import LatestBlog from './partials/LatestBlog.vue';
+import WorkTogether from './partials/WorkTogether.vue';
+import Newsletter from './partials/Newsletter.vue';
  export default {
   name:'Main',
   components:{
@@ -14,7 +16,9 @@ import LatestBlog from './partials/LatestBlog.vue';
     VideoSection,
     Testimonial,
     LatesCourses,
-    LatestBlog
+    LatestBlog,
+    WorkTogether,
+    Newsletter
   },
   data(){
     return{
@@ -26,70 +30,14 @@ import LatestBlog from './partials/LatestBlog.vue';
 
 <template>
   <main >
-    <Jumbotron/>
-    <ArtistCoaching/>
+    <Jumbotron title="Hello, I'm Martin"/>
+    <ArtistCoaching title="Artist Coaching"/>
     <VideoSection/>
-    <Testimonial/>
+    <Testimonial title="Testimonials"/>
     <LatesCourses title="Artist coaching" type="courses"/>
     <LatestBlog title="Articles and Tips" type="latestBlog"/>
-
-    
-
-    <!-- lavoriamo insieme -->
-    <section id="work-together">
-      <div class="rotate-text">
-        <span class="rotate">Events</span>
-      </div>
-      <div class="ac_sm-container">
-        <div class="title-section text-center">
-          <em>Upcoming Events</em>
-          <h4>Let's Work Together</h4>
-        </div>
-        <div class="row w-100">
-          <div 
-          v-for="(card,index) in store.workTogheter" :key="card + index"
-          class="col-6">
-          <div class="card mb-3" >
-            <div :class="{'flex-row-reverse': index >= store.workTogheter.length - 2}" class="row g-0">
-              <div class="col-md-4">
-                <img :src="`/images/${card.imgCourse}`" class="img-fluid" alt="...">
-              </div>
-              <div class="col-md-8">
-                <div :class="{'text-end': index >= store.workTogheter.length - 2}" class="card-body ">
-                  <span class="date">{{ card.date }}</span>
-                  <h6>{{ card.name }}</h6>
-                  <span class="location"><i class="fa-solid fa-location-dot"></i>{{ card.location }}</span>
-                </div>
-              </div>
-            </div>
-          </div>
-          </div>
-        </div>
-      </div>
-    </section>
-    
-
-    <!-- Newsletter -->
-    <section id="newsletter"> 
-      <div class="background-left"></div>
-      <div class="background-rigth"></div>
-      <div class="ac_sm-container h-100">
-        <div class="row h-100">
-          <div class="col-4 h-100 d-flex align-items-center">
-            <h4>Newsletter to get in touch</h4>
-          </div>
-          <div class="col-8 h-100 d-flex align-items-center">
-            <div class="mail-box">
-                <input type="text" placeholder="Your e-mail" style="color:white">
-                <i class="fa-solid fa-arrow-right-long text-end"></i>
-            </div>
-          </div>
-        </div>
-      </div>
-
-    </section>
-
-
+    <WorkTogether title="Upcoming Events"/>
+    <Newsletter title="Newsletter to get in touch"/>
   </main>
 </template>
 
@@ -97,94 +45,5 @@ import LatestBlog from './partials/LatestBlog.vue';
 <style lang="scss" scoped>
   @use '../../scss/partials/general'as *;
   @use '../../scss/partials/vars'as *;
-
-    // lavoriamo insieme
-    #work-together {
-      position: relative;
-      .rotate-text {
-        position: absolute;
-        bottom: 20%;
-        left: -100px;
-        transform: translate(-50% , -50%);
-        transform: rotate(90deg);
-        }
-      .card {
-        border: none;
-        .card-body{
-          padding: 30px 30px;
-          .date {
-            font-size: .8rem;
-            color: rgba(128, 128, 128, 0.644);
-          }
-          h6 {
-            font-weight: 600;
-          }
-          .location{
-            font-size: .7rem;
-            color: rgba(128, 128, 128, 0.487);
-            i {
-              padding: 0 5px;
-            }
-          }
-
-        }
-      }
-    }
-
-
-    // newsletter
-    #newsletter {
-      height: 100px;
-      background-color: rgb(36, 65, 134);
-      color: white;
-      position: relative;
-      .background-left {
-        width: 20%;
-        height: 100%;
-        position: absolute;
-        top:0;
-        left: 0;
-        background-image: url('/images/artist-shape-color-paint-top-left.png');
-        background-position: center;
-        background-size: cover;
-      }
-      .background-rigth {
-        width: 20%;
-        height: 100%;
-        position: absolute;
-        top:0;
-        right: 0;
-        background-image: url('/images/artist-shape-color-paint-bottom-right.png');
-        background-position: center;
-        background-size: cover;
-      }
-      .mail-box {
-        width: 100%;
-        border-bottom: 1px solid rgba(255, 255, 255, 0.387);
-        span {
-          width: 10%;
-        }
-        input {
-          width: 85%;
-          border:none;
-          background-color:  rgb(36, 65, 134);
-          padding: 5px;
-          border-radius: 0;
-          text-shadow: none;
-          box-shadow: none;
-          outline: none;
-          &::placeholder{
-            color: white;
-          }
-        }
-        i{
-          cursor: pointer;
-          width: 15%;
-          font-size: 1.3rem;
-        }
-        
-      }
-    }
-
 
 </style>
