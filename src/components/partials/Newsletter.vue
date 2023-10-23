11a6ea9f076d00eb7e474a3a4804e61629f023ec
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-23 12:46:55 +0200

    [ init ] component for main

---
+++ src/components/partials/Newsletter.vue
@@ -0,0 +1,103 @@
+<script>
+import { store } from '../../data/store';
+ export default {
+  name:'Newsletter',
+  data(){
+    return {
+      store,
+      mailNews:''
+    }
+  },
+  props:{
+    title:String
+  },
+  methods:{
+    getMail(){
+      store.newsletter.push( this.mailNews)
+      this.mailNews = ''
+      console.log(store.newsletter);
+    }
+  }
+}
+</script>
+
+<template>
+  <!-- Newsletter -->
+  <section id="newsletter"> 
+      <div class="background-left"></div>
+      <div class="background-rigth"></div>
+      <div class="ac_sm-container h-100">
+        <div class="row h-100">
+          <div class="col-4 h-100 d-flex align-items-center">
+            <h4>{{ title }}</h4>
+          </div>
+          <div class="col-8 h-100 d-flex align-items-center">
+            <div class="mail-box">
+                <input @keyup.enter="getMail" v-model.trim="mailNews" type="email" id="email" name="email" placeholder="Your e-mail" style="color:white">
+                <i @click="getMail" class="fa-solid fa-arrow-right-long text-end"></i>
+            </div>
+          </div>
+        </div>
+      </div>
+
+    </section>
+</template>
+
+
+<style lang="scss" scoped>
+ @use '../../../scss/partials/general' as *;
+ @use '../../../scss/partials/vars' as *;
+  // newsletter
+  #newsletter {
+      height: 150px;
+      background-color: rgb(36, 65, 134);
+      color: white;
+      position: relative;
+      .background-left {
+        width: 20%;
+        height: 100%;
+        position: absolute;
+        top:0;
+        left: 0;
+        background-image: url('/images/artist-shape-color-paint-top-left.png');
+        background-position: center;
+        background-size: cover;
+      }
+      .background-rigth {
+        width: 20%;
+        height: 100%;
+        position: absolute;
+        top:0;
+        right: 0;
+        background-image: url('/images/artist-shape-color-paint-bottom-right.png');
+        background-position: center;
+        background-size: cover;
+      }
+      .mail-box {
+        width: 100%;
+        border-bottom: 1px solid rgba(255, 255, 255, 0.387);
+        span {
+          width: 10%;
+        }
+        input {
+          width: 85%;
+          border:none;
+          background-color:  rgb(36, 65, 134);
+          padding: 5px;
+          border-radius: 0;
+          text-shadow: none;
+          box-shadow: none;
+          outline: none;
+          &::placeholder{
+            color: white;
+          }
+        }
+        i{
+          cursor: pointer;
+          width: 15%;
+          font-size: 1.3rem;
+        }
+        
+      }
+    }
+</style>
---
+++ src/components/partials/WorkTogether.vue
@@ -0,0 +1,87 @@
+<script>
+import { store } from '../../data/store';
+ export default {
+  name:'WorkTogether',
+  data(){
+    return {
+      store
+    }
+  },
+  props:{
+    title:String
+  }
+}
+</script>
+
+<template>
+  <!-- lavoriamo insieme -->
+  <section id="work-together">
+      <div class="rotate-text">
+        <span class="rotate">Events</span>
+      </div>
+      <div class="ac_sm-container">
+        <div class="title-section text-center">
+          <em>{{ title }}</em>
+          <h4>Let's Work Together</h4>
+        </div>
+        <div class="row w-100">
+          <div 
+          v-for="(card,index) in store.workTogheter" :key="card + index"
+          class="col-6">
+          <div class="card mb-3" >
+            <div :class="{'flex-row-reverse': index >= store.workTogheter.length - 2}" class="row g-0">
+              <div class="col-md-4">
+                <img :src="`/images/${card.imgCourse}`" class="img-fluid" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div :class="{'text-end': index >= store.workTogheter.length - 2}" class="card-body ">
+                  <span class="date">{{ card.date }}</span>
+                  <h6>{{ card.name }}</h6>
+                  <span class="location"><i class="fa-solid fa-location-dot"></i>{{ card.location }}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          </div>
+        </div>
+      </div>
+    </section>
+</template>
+
+
+<style lang="scss" scoped>
+ @use '../../../scss/partials/general' as *;
+ @use '../../../scss/partials/vars' as *;
+   // lavoriamo insieme
+   #work-together {
+      position: relative;
+      .rotate-text {
+        position: absolute;
+        bottom: 20%;
+        left: -100px;
+        transform: translate(-50% , -50%);
+        transform: rotate(90deg);
+        }
+      .card {
+        border: none;
+        .card-body{
+          padding: 30px 30px;
+          .date {
+            font-size: .8rem;
+            color: rgba(128, 128, 128, 0.644);
+          }
+          h6 {
+            font-weight: 600;
+          }
+          .location{
+            font-size: .7rem;
+            color: rgba(128, 128, 128, 0.487);
+            i {
+              padding: 0 5px;
+            }
+          }
+
+        }
+      }
+    }
+</style>
