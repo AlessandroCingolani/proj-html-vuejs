2e56c511620d17403fd9629ac679a11da26379ae
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-10-23 12:48:20 +0200

    [ add ] collage and margin

--- src/components/partials/VideoSection.vue
+++ src/components/partials/VideoSection.vue
@@ -1,16 +1,16 @@
 <script>
  export default {
-  name:'VideoSection'
+  name:'VideoSection',
 }
 </script>
 
 <template>
   <!-- sezione video -->
-  <section>
+  <section id="player-section">
       <div class="container-fluid">
         <div class="ac_sm-container">
 
-          <div class="row">
+          <div class="row mb-5">
 
             <div class="col-4 text-center">
               <img class="pb-3" src="/public/images/artist-quote-icon.png" alt="">
@@ -27,8 +27,15 @@
           </div>
           <!-- mettere poi icona youtube e decorazioni in absolute -->
           <div class="video-block">
+            <div class="collage">
+              <img class="shape-12" src="/public/images/maxcoach-shape-12-150x150.png" alt="">
+              <img class="shape-5" src="/public/images/maxcoach-shape-05.png" alt="">
+            </div>
             <img class="video" src="/public/images/artist-video-poster.jpg" alt="">
             <img class="youtube" src="/public/images/icon-youtube-play.png" alt="">
+          </div>
+          <div class="background">
+
           </div>
         </div>
       </div>
@@ -48,24 +55,70 @@
   color: rgba(128, 128, 128, 0.799);
 
  }
- .video-block {
-  position: relative;
-  margin: 0 auto;
-  width: 80%;
-  .video {
+
+ #player-section {
+  
+    position: relative;
+    padding-bottom:120px ;
+    
+    .background {
     width: 100%;
-  }
-  .youtube {
+    height: 300px;
     position: absolute;
-    cursor: pointer;
-    width: 10%;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50% , -50%);
-    transition: .4s all;
-    &:hover {
-      scale:1.2
-    }
+    bottom: -30px;
+    left: 0;
+    // background-image: url('/public/images/artist-shape-02.png');
+    // background-position-y: bottom;
+    // background-repeat: repeat-x;
+    // transform: rotate(180deg);
+    // background-image: url('/public/images/artist-shape-02.png');
+    // background-repeat: repeat, repeat;
+    // background-size: 200px, 500px;
+    // background-position: 0px 0px, 0px 0px;
+    // background-repeat-x: 0px, 1px;
   }
+   .video-block {
+    position: relative;
+    margin: 0 auto;
+    width: 80%;
+    .collage {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      .shape-12 {
+        position: absolute;
+        width: 20%;
+        bottom: -10px;
+        right: -100px;
+        z-index: 1;
+      }
+      .shape-5 {
+        position: absolute;
+        width: 20%;
+        top: -20px;
+        left: -30px;
+        z-index: 1;
+      }
+    }
+    .video {
+      position: relative;
+      width: 100%;
+      z-index: 10;
+    }
+    .youtube {
+      position: absolute;
+      cursor: pointer;
+      width: 10%;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50% , -50%);
+      transition: .4s all;
+      z-index: 11;
+      &:hover {
+        scale:1.2
+      }
+    }
+  
+ }
  }
 </style>
